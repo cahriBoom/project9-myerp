aa86eeb6a6d6d0854dff4343a10b1128469b49bb
cahriBoom <52654757+cahriBoom@users.noreply.github.com>
2021-03-24 17:27:38 +0100

    Add files via upload

---
+++ project9/src/myerp-consumer/src/test-consumer/com/dummy/myerp/testconsumer/consumer/ComptabiliteDaoImpIT.java
@@ -0,0 +1,13 @@
+package com.dummy.myerp.testconsumer.consumer;
+
+import org.junit.jupiter.api.Test;
+
+public class ComptabiliteDaoImpIT {
+
+	
+	@Test
+	public void testComptabiliteDao() {
+		
+	}
+	
+}
